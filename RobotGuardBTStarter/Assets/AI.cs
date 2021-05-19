8c0da3e1b4a8265b5aba87cf914eee08a63c72be
Gr1ffor <54276416+Gr1ffor@users.noreply.github.com>
2021-05-19 10:31:36 -0300

    IA Wander Panda

---
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+    // variaveis
+    public Transform player;
+    public Transform bulletSpawn;
+    public Slider healthBar;   
+    public GameObject bulletPrefab;
+
+    NavMeshAgent agent;
+    public Vector3 destination; // The movement destination.
+    public Vector3 target;      // The position to aim to.
+    float health = 100.0f;
+    float rotSpeed = 5.0f;
+
+    float visibleRange = 80.0f;
+    float shotRange = 40.0f;
+
+    void Start()
+    {
+        // chamando o componente
+        agent = this.GetComponent<NavMeshAgent>();
+        // desaceleração do tiro
+        agent.stoppingDistance = shotRange - 5; //for a little buffer
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+    void Update()
+    {
+        // barra de Vida
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        // valor da vida
+        healthBar.value = (int)health;
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+        // se a vida for menor que 100, você vai curar
+       if(health < 100)
+        health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // se collidir com a tag bullet perde vida
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+    }
+
+    [Task]
+    public void PickRandomDestination()
+    {
+        // escolhe um local aleatorio
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        // setta o destino
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void MoveToDestination()
+    {
+
+        if (Task.isInspected)
+        {
+            // timer
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        }
+        // se chegar na posição selecionada a task é um sucesso
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+}
+
---
+++ RobotGuardBTStarter/Assets/Drive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+    // variaveis
+	float speed = 20.0F;
+    float rotationSpeed = 120.0F;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update() {
+
+        // movimentação do tanque
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        // se aperta space, atira
+        if(Input.GetKeyDown("space"))
+        {
+            // cria o tiro
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            // adiciona força ao tiro
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+    }
+}
